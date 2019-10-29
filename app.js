4480cd34f3d61dd536c2e6d0d69de872bac0237b
MrFrancis94 <omfrancis94@hotmail.com>
2019-10-29 13:34:35 +0100

    .

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-$(() => {
-    $('.top h1').text('Traversing is fun');
-    $('#list').children().text('Hey!');
-    $('.element').children().addClass('hidden');
-    
-    $('.change-show').click(() => {
-    $('.show').toggleClass('hidden');
-    });
-    
-    $('.add-item').click(() => {
-        $('#list').append('<li>New Item</li>');
-    
-    }); 
-    }); // Document Ready Function aka DOM
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+
+changeColor.addEventListener("click", function(){
+    text.classList.add("change");
+});
